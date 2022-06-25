72bde90a73e541028b4fbbfe5f7067da5fa57d51
katmerugurcan <57903386+katmerugurcan@users.noreply.github.com>
2022-06-25 10:56:08 +0300

    Update storage.ts

--- src/assets/storage.ts
+++ src/assets/storage.ts
@@ -9,13 +9,3 @@ export const BG_IMAGES = [
     "https://wallpaperaccess.com/full/889731.jpg",
 ]
 // "https://wallpaperaccess.com/full/889732.jpg",
-export const STORAGE = [
-    { "taskId": 0, "taskName": "q" },
-    { "taskId": 1, "taskName": "w" },
-    { "taskId": 2, "taskName": "e" },
-    { "taskId": 3, "taskName": "r" },
-    { "taskId": 4, "taskName": "t" },
-    { "taskId": 5, "taskName": "y" },
-    { "taskId": 6, "taskName": "a" },
-    { "taskId": 7, "taskName": "s" },
-]
